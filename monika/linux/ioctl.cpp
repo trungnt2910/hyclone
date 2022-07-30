444591c01b993ee1b1f343ebf5fdf700ed3c247b
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2022-07-30 18:43:16 +0700

    feat(Monika): ioctl improvements
    - Monika does not complain anymore when termios.c_cflag contains
    RTSFLOW and CTSFLOW at the same time, as this can be emulated
    using Linux's CRTSCTS.
    - New Haiku specific ioctl implemented: B_GET_PATH_FOR_DEVICE.
    This allows the `less` tool to work properly.

--- monika/linux/ioctl.cpp
+++ monika/linux/ioctl.cpp
@@ -6,6 +6,7 @@
 #include "BeDefs.h"
 #include "errno_conversion.h"
 #include "export.h"
+#include "extended_commpage.h"
 #include "haiku_errors.h"
 #include "haiku_ioctl.h"
 #include "linux_debug.h"
@@ -15,6 +16,62 @@ typedef struct ktermios linux_termios;
 
 static void TermiosLinuxToB(const linux_termios& linuxTermios, haiku_termios& haikuTermios);
 static void TermiosBToLinux(const haiku_termios& haikuTermios, linux_termios& linuxTermios);
+
+enum
+{
+    B_GET_DEVICE_SIZE = 1,          /* get # bytes - returns size_t in *data */
+    B_SET_DEVICE_SIZE,              /* set # bytes - passes size_t in *data */
+    B_SET_NONBLOCKING_IO,           /* set to non-blocking i/o */
+    B_SET_BLOCKING_IO,              /* set to blocking i/o */
+    B_GET_READ_STATUS,              /* check if can read w/o blocking */
+                                    /* returns bool in *data */
+    B_GET_WRITE_STATUS,             /* check if can write w/o blocking */
+                                    /* returns bool in *data */
+    B_GET_GEOMETRY,                 /* get info about device geometry */
+                                    /* returns struct geometry in *data */
+    B_GET_DRIVER_FOR_DEVICE,        /* get the path of the executable serving */
+                                    /* that device */
+    B_GET_PARTITION_INFO,           /* get info about a device partition */
+                                    /* returns struct partition_info in *data */
+    B_SET_PARTITION,                /* obsolete, will be removed */
+    B_FORMAT_DEVICE,                /* low-level device format */
+    B_EJECT_DEVICE,                 /* eject the media if supported */
+    B_GET_ICON,                     /* return device icon (see struct below) */
+    B_GET_BIOS_GEOMETRY,            /* get info about device geometry */
+                                        /* as reported by the bios */
+                                        /*   returns struct geometry in *data */
+    B_GET_MEDIA_STATUS,             /* get status of media. */
+                                        /* return status_t in *data: */
+                                        /* B_NO_ERROR: media ready */
+                                        /* B_DEV_NO_MEDIA: no media */
+                                        /* B_DEV_NOT_READY: device not ready */
+                                        /* B_DEV_MEDIA_CHANGED: media changed */
+                                        /*  since open or last B_GET_MEDIA_STATUS */
+                                        /* B_DEV_MEDIA_CHANGE_REQUESTED: user */
+                                        /*  pressed button on drive */
+                                        /* B_DEV_DOOR_OPEN: door open */
+    B_LOAD_MEDIA,                   /* load the media if supported */
+    B_GET_BIOS_DRIVE_ID,            /* get bios id for this device */
+    B_SET_UNINTERRUPTABLE_IO,       /* prevent cntl-C from interrupting i/o */
+    B_SET_INTERRUPTABLE_IO,         /* allow cntl-C to interrupt i/o */
+    B_FLUSH_DRIVE_CACHE,            /* flush drive cache */
+    B_GET_PATH_FOR_DEVICE,          /* get the absolute path of the device */
+    B_GET_ICON_NAME,                /* get an icon name identifier */
+    B_GET_VECTOR_ICON,              /* retrieves the device's vector icon */
+    B_GET_DEVICE_NAME,              /* get name, string buffer */
+    B_TRIM_DEVICE,                  /* trims blocks, see fs_trim_data */
+
+    B_GET_NEXT_OPEN_DEVICE = 1000,  /* obsolete, will be removed */
+    B_ADD_FIXED_DRIVER,             /* obsolete, will be removed */
+    B_REMOVE_FIXED_DRIVER,          /* obsolete, will be removed */
+
+    B_AUDIO_DRIVER_BASE = 8000,     /* base for codes in audio_driver.h */
+    B_MIDI_DRIVER_BASE = 8100,      /* base for codes in midi_driver.h */
+    B_JOYSTICK_DRIVER_BASE = 8200,  /* base for codes in joystick.h */
+    B_GRAPHIC_DRIVER_BASE = 8300,   /* base for codes in graphic_driver.h */
+
+    B_DEVICE_OP_CODES_END = 9999    /* end of Be-defined control ids */
+};
 
 extern "C"
 {
@@ -120,6 +177,30 @@ status_t MONIKA_EXPORT _kern_ioctl(int fd, uint32 op, void* buffer, size_t lengt
             }
             return B_OK;
         }
+        case B_GET_PATH_FOR_DEVICE:
+        {
+            char* path = (char*)buffer;
+            long realLength =
+                GET_HOSTCALLS()->vchroot_unexpandat(fd, "", path, length);
+
+            if (realLength > length)
+            {
+                return B_BUFFER_OVERFLOW;
+            }
+
+            if (realLength < 0)
+            {
+                return B_BAD_VALUE;
+            }
+
+            if (realLength > 1 && path[realLength - 1] == '/')
+            {
+                path[realLength - 1] = '\0';
+                --realLength;
+            }
+
+            return realLength;
+        }
         STUB_IOCTL(FIONBIO);
         STUB_IOCTL(FIONREAD);
         STUB_IOCTL(FIOSEEKDATA);
@@ -149,6 +230,11 @@ status_t MONIKA_EXPORT _kern_ioctl(int fd, uint32 op, void* buffer, size_t lengt
         STUB_IOCTL(TIOCMBIS);
         STUB_IOCTL(TIOCMBIC);
         STUB_IOCTL(TIOCGSID);
+        default:
+            // Trace this, until we have better tools
+            // such as strace on Hyclone.
+            trace("Unknown ioctl.");
+            return B_BAD_VALUE;
     }
 
     return B_OK;
@@ -271,5 +357,18 @@ static void TermiosBToLinux(const haiku_termios& haikuTermios, linux_termios& li
 
         linuxTermios.c_ispeed = 31250;
         linuxTermios.c_ospeed = 31250;
+    }
+
+    // Linux has no separate RTS and CTS flow control.
+    if ((haikuTermios.c_cflag & HAIKU_RTSFLOW) &&
+        !(haikuTermios.c_cflag & HAIKU_CTSFLOW))
+    {
+        trace("Unsupported c_cflag: RTSFLOW without CTSFLOW");
+    }
+
+    if ((haikuTermios.c_cflag & HAIKU_CTSFLOW) &&
+        !(haikuTermios.c_cflag & HAIKU_RTSFLOW))
+    {
+        trace("Unsupported c_cflag: CTSFLOW without RTSFLOW");
     }
 }
