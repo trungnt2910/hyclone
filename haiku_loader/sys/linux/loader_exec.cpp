24e0f55805bce67224ddba06a1c366cd968d3e47
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2023-04-13 19:29:35 +1000

    fix(loader): exec improvements
    - `loader_exec` now uses the new `vchroot_expandat` servercall,
    instead of the old and potentially buggy loader function.
    - `loader_exec` now expands argv[0] and handles the $PATH
    environment variable when a native binary is detected.

--- haiku_loader/sys/linux/loader_exec.cpp
+++ haiku_loader/sys/linux/loader_exec.cpp
@@ -9,9 +9,11 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <unordered_set>
 
 #include "BeDefs.h"
 #include "haiku_errors.h"
+#include "haiku_fcntl.h"
 #include "loader_debugger.h"
 #include "loader_elf.h"
 #include "loader_exec.h"
@@ -20,30 +22,39 @@
 
 const unsigned char s_ElfMagic[] = { 0x7f, 'E', 'L', 'F' };
 
-int (*loader_haiku_test_executable)(const char *name, char *invoker) = NULL;
+int (*loader_haiku_test_executable)(const char* name, char* invoker) = NULL;
 
-static bool IsHaikuExecutable(Elf64_Ehdr_ *ehdr, size_t size) {
-  int i, n;
-  bool res = false;
-  const char *stab;
-  const Elf64_Sym_ *st;
-  if ((stab = GetElfStringTable(ehdr, size)) &&
-      (st = GetElfSymbolTable(ehdr, size, &n))) {
-    for (i = 0; i < n; ++i) {
-      if (ELF64_ST_TYPE_(st[i].info) == STT_OBJECT_ &&
-          !strcmp(stab + Read32(st[i].name), "_gSharedObjectHaikuVersion")) {
-        res = true;
-        break;
-      }
+static bool IsHaikuExecutable(Elf64_Ehdr_* ehdr, size_t size)
+{
+    int i, n;
+    bool res = false;
+    const char *stab;
+    const Elf64_Sym_* st;
+    if ((stab = GetElfStringTable(ehdr, size)) &&
+        (st = GetElfSymbolTable(ehdr, size, &n)))
+    {
+        for (i = 0; i < n; ++i)
+        {
+            if (ELF64_ST_TYPE_(st[i].info) == STT_OBJECT_ &&
+                !strcmp(stab + Read32(st[i].name), "_gSharedObjectHaikuVersion"))
+            {
+                res = true;
+                break;
+            }
+        }
     }
-  }
-  return res;
+    return res;
 }
 
 int loader_exec(const char* path, const char* const* flatArgs, size_t flatArgsSize, int argc, int envc, int umask)
 {
+    if (path == NULL)
+    {
+        return B_BAD_ADDRESS;
+    }
+
     char hostPath[PATH_MAX];
-    loader_vchroot_expand(path, hostPath, sizeof(hostPath));
+    loader_hserver_call_vchroot_expandat(HAIKU_AT_FDCWD, path, strlen(path), true, hostPath, sizeof(hostPath));
 
     // Despite having a hook of Haiku's test_executable function,
     // we still have to conduct our own check as the function
@@ -60,7 +71,7 @@ int loader_exec(const char* path, const char* const* flatArgs, size_t flatArgsSi
             {
                 if (Read32(tmp) == Read32(s_ElfMagic))
                 {
-                    isHaikuExecutable = IsHaikuExecutable((Elf64_Ehdr_ *)tmp, statbuf.st_size);
+                    isHaikuExecutable = IsHaikuExecutable((Elf64_Ehdr_*)tmp, statbuf.st_size);
                 }
                 else
                 {
@@ -101,11 +112,88 @@ int loader_exec(const char* path, const char* const* flatArgs, size_t flatArgsSi
         const char* const* envp = flatArgs + argc + 1;
 
         status = execve(loaderPath.c_str(), (char* const*)argv, (char* const*)envp);
+
+        delete[] argv;
     }
     else
     {
-        const char* const* envp = flatArgs + argc + 1;
-        status = execve(hostPath, (char* const*)flatArgs, (char* const*)envp);
+        const char** argv;
+
+        if (flatArgs[0] && flatArgs[0][0] == '/')
+        {
+            argv = new const char*[argc + 1];
+            std::copy(flatArgs, flatArgs + argc, argv);
+            argv[argc] = NULL;
+
+            char* hostArgv0 = new char[PATH_MAX];
+            loader_hserver_call_vchroot_expandat(HAIKU_AT_FDCWD, flatArgs[0], strlen(flatArgs[0]), true, hostArgv0, PATH_MAX);
+
+            argv[0] = hostArgv0;
+        }
+        else
+        {
+            argv = (const char **)flatArgs;
+        }
+
+        int envc = 0;
+        while (flatArgs[argc + 1 + envc])
+        {
+            ++envc;
+        }
+
+        const char** envp = new const char*[envc + 1];
+        std::copy(flatArgs + argc + 1, flatArgs + argc + 1 + envc, envp);
+        envp[envc] = NULL;
+
+        std::string hostPathEnv = "PATH=";
+
+        for (int i = 0; i < envc; ++i)
+        {
+            if (strncmp(envp[i], "PATH=", 5) == 0)
+            {
+                std::stringstream hPathStream(getenv("HPATH"));
+                std::unordered_set<std::string> hPaths;
+
+                std::string currentPath;
+                while (std::getline(hPathStream, currentPath, ':'))
+                {
+                    hPaths.insert(currentPath);
+                }
+
+                std::stringstream ss(envp[i] + 5);
+                char currentHostPath[PATH_MAX];
+
+                while (std::getline(ss, currentPath, ':'))
+                {
+                    if (hPaths.contains(currentPath))
+                    {
+                        continue;
+                    }
+
+                    loader_hserver_call_vchroot_expandat(HAIKU_AT_FDCWD, currentPath.c_str(), currentPath.length(), true,
+                        currentHostPath, sizeof(currentHostPath));
+
+                    if (hostPathEnv.length() > 0)
+                    {
+                        hostPathEnv += ":";
+                    }
+
+                    hostPathEnv += currentHostPath;
+                }
+
+                envp[i] = hostPathEnv.c_str();
+            }
+        }
+
+        status = execve(hostPath, (char* const*)argv, (char* const*)envp);
+
+        if (flatArgs[0] && flatArgs[0][0] == '/')
+        {
+            delete[] argv[0];
+            delete[] argv;
+        }
+
+        delete[] envp;
     }
 
     loader_hserver_call_exec(false);
